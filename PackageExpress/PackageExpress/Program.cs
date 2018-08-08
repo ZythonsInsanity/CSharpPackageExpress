fefdc59d15a5be78a17240c8a5039843379eec4c
ZythonsInsanity <37195840+ZythonsInsanity@users.noreply.github.com>
2018-08-08 16:57:22 -0700

    Re-re uploading officially correct assignment

---
+++ PackageExpress/PackageExpress/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Package Express. Press 'enter' and follow the instructions below.");
+        Console.ReadLine();
+        Console.WriteLine("Please enter the package weight:");
+        int packageWeight = Convert.ToInt32(Console.ReadLine());
+        if (packageWeight > 50)
+        {
+            Console.WriteLine("Your package is too heavy to be shipped via Package Express. Have a good day!");
+            Console.ReadLine();
+        }
+        else
+        {
+            Console.WriteLine("Please enter the package width:");
+            int packageWidth = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter package height");
+            int packageHeight = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter package length");
+            int packageLength = Convert.ToInt32(Console.ReadLine());
+            int dimensionsTotal = packageWidth * packageHeight * packageLength;
+
+            if (dimensionsTotal > 50)
+            {
+                Console.WriteLine("Your package is too big to be shipped via Package Express.");
+            }
+            else
+            {
+                Console.WriteLine("Your estimated total for shipping this package is: {0:C}", Convert.ToDecimal(dimensionsTotal) * packageWeight / 100);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
